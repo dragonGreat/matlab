8ebff208c6efea68d7c5a2fe962dec955d297ae4
dragon <1011151667@qq.com>
2015-10-21 17:07:17 +0800

    plot Power and PowerFator

--- smartMeter/main.m
+++ smartMeter/main.m
@@ -3,50 +3,24 @@ clear;
 clc;
 fprintf('main  function lets go ! ... ...\n');
 isGetDataToMat=0;%0不更新all.mat中数据，1再次更新all.mat中的数据 ，在没有新数据的情况下只要在第一次使用时赋1,默认为0
-getDataToMat(isGetDataToMat);
+getDataToMat(isGetDataToMat);%将数据从excel中转到mat中
 allData=load('all.mat');
-bus=allData.buscell;%获取电扇每步数据
-x=2;
-figure(1);
-    subplot(2,3,1);
-        plot(1:length(bus{1,x}{1,1}),bus{1,x}{1,1}/10,'r');
-                title('Power');
-                xlabel('number');
-                ylabel('value');
-                legend('P','location','Best');
-    subplot(2,3,2);
-        plot(1:length(bus{1,x}{1,2}),bus{1,x}{1,2}/1000,'b');
-                title('PowerFactor');
-                xlabel('number');
-                ylabel('value');
-                legend('PF','location','Best');
-                
-    subplot(2,3,3);
-        plot(1:length(bus{1,x}{1,3}),bus{1,x}{1,3}/10,'g');
-                title('voltage');
-                xlabel('number');
-                ylabel('value');
-                legend('U','location','Best');
-               
-    subplot(2,3,4);
-        boxplot(bus{1,x}{1,3}/10);
-        title('voltage box');
+fan=allData.fancell;%获取电扇每步数据
+miphone=allData.miphonecell;%获取手机每步数据
+monitor=allData.monitorcell;%获取显示器每步数据
+bus=allData.buscell;%获取总线每步数据
 
-                
-     subplot(2,3,5);
-        plot(1:length(bus{1,x}{1,4}),bus{1,x}{1,4}/1000,'k');
-                title('current');
-                xlabel('number');
-                ylabel('value');
-                legend('I','location','Best');             
-                
-     subplot(2,3,6);
-        plot(1:length(bus{1,x}{1,1}),bus{1,x}{1,1},'r',1:length(bus{1,x}{1,2}),bus{1,x}{1,2},'b',1:length(bus{1,x}{1,3}),bus{1,x}{1,3},'k',1:length(bus{1,x}{1,4}),bus{1,x}{1,4},'g');
-                title('all(P,PF,U,I)');
-                xlabel('number');
-                ylabel('value');
-                legend('P','PF','U','I','location','Best');    
-       
+fanP=bus{1,2}{1,1};
+fanPF=bus{1,2}{1,2};
+A=[fanP,fanPF];
+figure(1)
+hist(fan{1,1}{1,1},30);
+figure(2)
+hist(fan{1,1}{1,2},30);
+figure(3)
+mesh(A)
+
+              
 
 fprintf('main function fun over!\n');
 
@@ -119,3 +93,51 @@ fprintf('main function fun over!\n');
 % plot(f,mag)
 % % % % % % % *********************************************************************************************% % %
 
+
+% figure(1);
+%     subplot(2,3,1);
+%         plot(fan{1,1}{1,1},fan{1,1}{1,2},'ro');
+%                 title('fan');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%                % legend('P','location','Best');
+%     subplot(2,3,2);
+%         plot(fan{1,2}{1,1},fan{1,2}{1,2},'bo');
+%                 title('fan');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%     subplot(2,3,3);
+%         plot(miphone{1,1}{1,1},miphone{1,1}{1,2},'ro');
+%                 title('mi');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%                % legend('P','location','Best');
+%     subplot(2,3,4);
+%         plot(miphone{1,2}{1,1},miphone{1,2}{1,2},'bo');
+%                 title('mi');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%      subplot(2,3,5);
+%         plot(monitor{1,1}{1,1},monitor{1,1}{1,2},'ro');
+%                 title('monitor');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%                % legend('P','location','Best');
+%     subplot(2,3,6);
+%         plot(monitor{1,2}{1,1},monitor{1,2}{1,2},'bo');
+%                 title('monitor');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+% figure(2);
+%         plot(fan{1,3}{1,1},fan{1,3}{1,2},'bo',miphone{1,2}{1,1},miphone{1,2}{1,2},'ro',monitor{1,2}{1,1},monitor{1,2}{1,2},'go');
+%                 title('风扇，小米手机，显示器');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+%                 legend('fan','miphone','monitor','location','Best');
+% 
+%  figure(3);
+%          plot(bus{1,2}{1,1},bus{1,2}{1,2},'ro');
+%                 title('bus');
+%                 xlabel('power/W');
+%                 ylabel('PF');
+ % % % % % % % *********************************************************************************************% % %
